e98db4365efdb63053fce9771f8c2624395f62f8
Railey <RaileyPacheco099@Gmail.com>
2024-01-29 02:49:51 -0800

    Update code in FormStudent component and Flask backend

--- app/FormStudent/backend/sever.py
+++ app/FormStudent/backend/sever.py
@@ -2,7 +2,7 @@ from flask import Flask, jsonify, request, json
 from flask_cors import CORS
 
 app = Flask(__name__)
-CORS(app)
+CORS(app, resources={r"/studentForm": {"origins": "https://for-student-gt2errli3-railey29s-projects.vercel.app"}})
 
 @app.route("/studentForm", methods=["GET", "POST"])
 def get_data():
