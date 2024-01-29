2307f97453c47aa15fb3e02e4f52167cad97a2cf
Railey <RaileyPacheco099@Gmail.com>
2024-01-29 05:22:11 -0800

    Update code in FormStudent component and Flask backend

--- app/FormStudent/Frontend/components/FormStudent.jsx
+++ app/FormStudent/Frontend/components/FormStudent.jsx
@@ -55,7 +55,7 @@ function FormStudent() {
       console.log("Sending POST request..."); // console For Error
       // API or EndPoint
       const response = await axios.post(
-        "http://192.168.5.213:5000/studentForm", // or use the correct URL for your Flask server
+        "https://192.168.5.213:5000/studentForm", // or use the correct URL for your Flask server
         {
           name: userInput,
           address: userAddress,
