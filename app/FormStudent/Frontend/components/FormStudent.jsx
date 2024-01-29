218397682068db65ae4fced1324f05cda2e0f8bd
Railey <RaileyPacheco099@Gmail.com>
2024-01-29 02:44:43 -0800

    Update code in FormStudent component and Flask backend

--- app/FormStudent/Frontend/components/FormStudent.jsx
+++ app/FormStudent/Frontend/components/FormStudent.jsx
@@ -55,7 +55,7 @@ function FormStudent() {
       console.log("Sending POST request..."); // console For Error
       // API or EndPoint
       const response = await axios.post(
-        "https://for-student-taupe.vercel.app/",
+        "https://for-student-taupe.vercel.app/studentForm",
         {
           // Add Data you want to pass into Flask
           // key , value(ito yung Variable sa useState)
