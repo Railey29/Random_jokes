e2682f231ef07a4dbde38f9a328328bc52adb55a
Railey <RaileyPacheco099@Gmail.com>
2024-01-29 02:53:45 -0800

    Update code in FormStudent component and Flask backend

--- app/FormStudent/Frontend/components/FormStudent.jsx
+++ app/FormStudent/Frontend/components/FormStudent.jsx
@@ -66,7 +66,7 @@ function FormStudent() {
       );
       setData(response.data); // ifefetch nung frontEnd yung Data After ma process ng Flask And ito yung kukuha ng mga data ni user
     } catch (error) {
-      console.error("Have an Error!");
+      console.error("Have an Error!", error);
     }
   };
 
