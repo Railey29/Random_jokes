64bd45129caa24490df8b5f516870222397d5217
Railey <RaileyPacheco099@Gmail.com>
2024-01-29 01:19:47 -0800

    Update styles

---
+++ app/style/global-style.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const GlobalStyle = () => (
+  <style jsx global>{`
+    body {
+      padding: 0;
+      margin: 0;
+      background-color: #1f2421;
+    }
+  `}</style>
+);
+
+export default GlobalStyle;
